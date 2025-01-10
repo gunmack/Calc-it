d04d31063497066ddecd4b6cd33ba75176b35896
gunmack <jreedoy2002@yahoo.com>
2025-01-10 01:14:01 -0800

    organized into folders

---
+++ app/app.py
@@ -0,0 +1,32 @@
+from flask import Flask, render_template, request, redirect, url_for
+from functions import evaluate_expression
+
+app = Flask(__name__)
+
+@app.route("/", methods=["GET", "POST"])
+def numberpad():
+    expression = request.form.get("expression", "")
+    result = ""
+    recall = expression [:]
+
+    if request.method == "POST":
+        if 'delete' in request.form:
+            expression = expression[:-1]
+        elif 'clear' in request.form:
+            expression = ""
+            result + ""
+        elif 'recall' in request.form:
+            expression = recall [:]
+        else:
+            if expression:
+                try:
+                    result = evaluate_expression(expression)
+                    expression = result
+                    recall = expression [:]
+                except Exception as e:
+                    result = "Error"
+
+    return render_template('index.html', expression=expression)
+
+if __name__ == "__main__":
+    app.run()
